7d712015e56ba576b2a129803156a86e8b1e71e2
ankur-kakrecha <ankurk.reliution@gmail.com>
2024-01-11 11:27:42 +0530

    some changs in  Exercise E001-V1

--- Exercise_1.py
+++ Exercise_1.py
@@ -19,21 +19,80 @@ class category:
         print("no_of_products :", self.no_of_products)
 
 class product():
-    counter=0
     def __init__(self,p_name,p_code,category,price):
         self.p_name = p_name
         self.p_code = p_code
         self.category = category
         self.price = price
-        product.counter=product.counter+1
-    
+        
     def print_product(self):
         print("Product name: " , self.p_name)
         print("Product code: ", self.p_code)
         print("Category: ", self.category)
         print("Price: ", self.price)
+    
+    @classmethod    
+    def product_search(self,lst):
+        flag=True
+        c=0
+        find_product=input("enter a code to search for products :")
+        for i in lst:
+            if i.p_code == find_product:
+                flag=True
+                break
+            else:
+                flag=False
+            c=c+1
+        if flag==True:
+            print("product name :",lst[c].p_name)
+            print("product code :",lst[c].p_code)
+            print("product category :",lst[c].category)
+            print("product price :",lst[c].price)
+        else:
+            print("product not found")
+        
+    @classmethod
+    def price_low_to_high(self,lst):
+        for i in range(0,len(lst)):
+            for j in range(i+1,len(lst)):
+                if lst[i].price > lst[j].price:
+                    temp=lst[i].price
+                    lst[i].price=lst[j].price
+                    lst[j].price=temp
+        for i in lst:
+            i.print_product()
+            # print(lst[i].price)
+    
+    @classmethod
+    def price_high_to_low(self,lst):
+        for i in range(0,len(lst)):
+            for j in range(i+1,len(lst)):
+                if lst[i].price < lst[j].price:
+                    temp=lst[i].price
+                    lst[i].price=lst[j].price
+                    lst[j].price=temp
+        for i in lst:
+            i.print_product()
+        else:
+            print()
+            # print(lst[i].price)
 
 
+# create object of category and print details
+print("\n==============Category==============\n")
+c_count=0
+t_count=0
+b_count=0
+c1=category('car','c001',c_count)
+c2=category('truck','t001',t_count)
+c3=category('bike','b001',b_count)
+cat_list=[c1,c2,c3]
+for i in cat_list:
+    i.print_category_details()
+else:
+    print()
+
+# create objects of products     
 print("Products\n")
 p1=product('ashok layland','a01','truck',1200000)        
 p2=product('bmw','bm01','car',200000)
@@ -46,24 +105,10 @@ p8=product('shine','sp01','bike',90000)
 p9=product('g-wagon','g01','car',920000)
 p10=product('safari','sf1','car',800000)
 p11=product('i10','i01','car',240000)
-
-# print(product.counter)
-
-# product_list=[]
-
-# for i in range(1,product.counter+1):
-#     product_list.append("p{0}".format(i))
-# print(product_list)
     
 product_list=[p1, p2, p3, p4, p5, p6,p7, p8, p9, p10,p11]
 
-for i in product_list:
-    i.print_product()
-else:
-    print()
-c_count=0
-t_count=0
-b_count=0
+# count the product of perticuler category
 for i in product_list:
     if i.category == 'car':
         c_count+=1
@@ -71,60 +116,21 @@ for i in product_list:
         t_count+=1
     elif i.category == 'bike':
         b_count+=1
-print("\nCategory\n")
-c1=category('car','c001',c_count)
-c2=category('truck','t001',t_count)
-c3=category('bike','b001',b_count)
-cat_list=[c1,c2,c3]
-for i in cat_list:
-    i.print_category_details()
+
+#print products details
+   
+for i in product_list:
+    i.print_product()
 else:
     print()
+    
+# search product thrue their product code
+product.product_search(product_list)
 
-def product_search():
-    flag=True
-    c=0
-    find_product=input("enter a code to search for products :")
-    for i in product_list:
-        if i.p_code == find_product:
-            flag=True
-            break
-        else:
-            flag=False
-        c=c+1
-    if flag==True:
-        print("product name :",product_list[c].p_name)
-        print("product code :",product_list[c].p_code)
-        print("product category :",product_list[c].category)
-        print("product price :",product_list[c].price)
-    else:
-        print("product not found")
-product_search()
+# low to high order base of price
+print("========== low to high price =============")
+product.price_low_to_high(product_list)
 
-def price_low_to_high():
-    for i in range(0,len(product_list)):
-        for j in range(i+1,len(product_list)):
-            if product_list[i].price > product_list[j].price:
-                temp=product_list[i].price
-                product_list[i].price=product_list[j].price
-                product_list[j].price=temp
-    for i in product_list:
-        i.print_product()
-        # print(product_list[i].price)
-
-def price_high_to_low():
-    for i in range(0,len(product_list)):
-        for j in range(i+1,len(product_list)):
-            if product_list[i].price < product_list[j].price:
-                temp=product_list[i].price
-                product_list[i].price=product_list[j].price
-                product_list[j].price=temp
-    for i in product_list:
-        i.print_product()
-    else:
-        print()
-        # print(product_list[i].price)
-print("\nlow to high price order\n")
-price_low_to_high()
-print("\nhigh to low price order\n")   
-price_high_to_low()
+# high to low order base of price
+print("==========  high price to low =============")
+product.price_high_to_low(product_list)
