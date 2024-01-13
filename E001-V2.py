524ed14a56fb09ab24bb2ae4038f77bf384b57cf
ankur-kakrecha <ankurk.reliution@gmail.com>
2024-01-13 10:18:37 +0530

    Exercise E001-v2 completed

--- E001-V2.py
+++ E001-V2.py
@@ -12,11 +12,10 @@
 # •  Display product list by category (group by category, order by category name).
 
 class category:
-    def __init__(self,c_name,c_code,parent=None,parent_name=None):
+    def __init__(self,c_name,c_code,parent=None):
         self.c_name = c_name
         self.c_code = c_code
         self.parent = parent
-        self.parent_name = parent_name
         self.no_of_product = 0
         self.products=[]
     def generate_display_names(self):
@@ -24,18 +23,15 @@ class category:
         if self.parent:
             display_name=self.parent.generate_display_names() + ">" + display_name
         return display_name
-    def generate_parent_names(self):
-        parent_name = self.c_name
+    # def show_category(self):
+    #     print("category name : {0} , category code : {1} , display name : {2} ,number of product {3} , parent : {4}".format(self.c_name, \
+    #         self.c_code, self.generate_display_names(), self.no_of_product,self.parent_name))
+    def __str__(self):
         if self.parent:
-            parent_name=self.parent.generate_display_names()
+            return f" category name = {self.c_name} , diaplay name = {self.generate_display_names()}, number of product ={self.no_of_product}, parent name= {self.parent.c_name}"
         else:
-            parent_name=None
-        return parent_name
-    def show_category(self):
-        print("category name : {0} , category code : {1} , display name : {2} ,number of product {3} , parent : {4}".format(self.c_name, \
-            self.c_code, self.generate_display_names(), self.no_of_product,self.parent_name))
-    # def __str__(self):
-    #     return f"parent name==================== : {self.parent}"
+            return f" category name = {self.c_name} , diaplay name = {self.generate_display_names()}, number of product ={self.no_of_product}, parent name= None"
+            
 
 class Product:
     def __init__(self,p_name,p_code,category,price):
@@ -62,16 +58,9 @@ class Product:
                 
                 
 vehicle=category("vehicle","v01")
-# print(vehicle.__str__())
-car=category("car","c01",vehicle,vehicle.c_name)
-truck=category("truck","t01",vehicle,vehicle.c_name)
-petrol=category("petrol","p01",car,car.c_name)
-# petrol2=category("petrol","p02",truck)
-# print(vehicle.generate_display_names())
-# print(car.generate_display_names())
-# print(truck.generate_display_names())
-# print(petrol.generate_display_names())
-# print(petrol2.generate_display_names())
+car=category("car","c01",vehicle)
+truck=category("truck","t01",vehicle)
+petrol=category("petrol","p01",car)
 
 vehicle.products.append(Product('vehicle1','v001',vehicle,120000))
 vehicle.products.append(Product('vehicle2','v002',vehicle,110000))
@@ -93,31 +82,12 @@ category_list=[vehicle,car,truck,petrol]
 # Product_list=[c1,c2,c3,t1,t2,t3,p1,p2,p3]
 for i in category_list:
     print("==============Category details==============\n")
-    i.show_category()
+    # i.show_category()
+    print(i)
     # print(i.c_name,i.no_of_product,i.c_code,)
     print("product of",i.c_name,":\n")
     for j in i.products:
         j.show_product()     
     print()
-# for i in category_list:
-#     i.show_category()
-#     print("product of",i.c_name,":\n")
-#     for j in i.products:
-#         for k in range(0,len(i.products)):
-#             for l in range(k+1,len(i.products)):
-#                 if i.products[k].p_name > i.products[l].p_name:
-#                     temp=i.products[k].p_name
-#                     i.products[k].p_name=i.products[l].p_name
-#                     i.products[l].p_name=temp
-#         j.show_products()
 print("============sorting============")
 Product.sort(category_list)
-
-        
-
-        
-            
-
-
-        
-        
